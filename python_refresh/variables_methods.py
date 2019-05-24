07311d1ca3e902d63d40d653687c7abccb1c63d4
Joe <joebarcus981@gmail.com>
2019-05-23 20:09:51 -0500

    first commit

---
+++ python_refresh/variables_methods.py
@@ -0,0 +1,11 @@
+a = 5
+b = 10
+my_variable = 56
+any_varable_name = 10
+
+string_variable = "hello"
+
+print(my_variable)
+
+def my_print_method():
+    
---
+++ sql_database/venv/lib/python3.7/_collections_abc.py
@@ -0,0 +1,1 @@
+/home/joe/anaconda3/lib/python3.7/_collections_abc.py
